cd16de6f775cf2d844b5841a2e3a81cd48171e20
Brandon Paredes <brandonparedes21@outlook.com>
2025-04-28 17:05:16 -0600

    Clases de PersonaRDAL y BL creadas

--- R.AccesoDatos/PersonaRDAL.cs
+++ R.AccesoDatos/PersonaRDAL.cs
@@ -1,4 +1,5 @@
-﻿using R.EntidadesNegocio;
+﻿using Microsoft.EntityFrameworkCore;
+using R.EntidadesNegocio;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,90 @@ namespace R.AccesoDatos
         public PersonaRDAL(RDbContext RDB)
         {
             dbContext = RDB;
+        }
+
+        public async Task<int> CrearAsync(PersonaR pPersonaR)
+        {
+            PersonaR personaR = new PersonaR()
+            {
+                NombreR = pPersonaR.NombreR,
+                ApellidoR = pPersonaR.ApellidoR,
+                FechaNacimientoR = pPersonaR.FechaNacimientoR,
+                SueldoR = pPersonaR.SueldoR,
+                EstatusR = pPersonaR.EstatusR
+            };
+            dbContext.PersonaR.Add(personaR);
+            return await dbContext.SaveChangesAsync();
+        }
+
+        public async Task<int> ModificarAsync(PersonaR pPersonaR)
+        {
+            var personaR = await dbContext.PersonaR.FirstOrDefaultAsync(s => s.Id == pPersonaR.Id);
+            if (personaR != null && personaR.Id != 0)
+            {
+                personaR.NombreR = pPersonaR.NombreR;
+                personaR.ApellidoR = pPersonaR.ApellidoR;
+                personaR.FechaNacimientoR = pPersonaR.FechaNacimientoR;
+                personaR.SueldoR = pPersonaR.SueldoR;
+                personaR.EstatusR = pPersonaR.EstatusR;
+
+                dbContext.Update(personaR);
+                return await dbContext.SaveChangesAsync();
+            }
+            else
+                return 0;
+        }
+
+        public async Task<int> EliminarAsync(PersonaR pPersonaR)
+        {
+            var personaR = await dbContext.PersonaR.FirstOrDefaultAsync(s => s.Id == pPersonaR.Id);
+            if (personaR != null && personaR.Id != 0)
+            {
+                dbContext.PersonaR.Remove(personaR);
+                return await dbContext.SaveChangesAsync();
+            }
+            else
+                return 0;
+        }
+
+        public async Task<PersonaR> ObtenerPorIdAsync(PersonaR pPersonaR)
+        {
+            var personaR = await dbContext.PersonaR.FirstOrDefaultAsync(s => s.Id == pPersonaR.Id);
+            if (personaR != null && personaR.Id != 0)
+            {
+                return new PersonaR
+                {
+                    Id = personaR.Id,
+                    NombreR = personaR.NombreR,
+                    ApellidoR = personaR.ApellidoR,
+                    FechaNacimientoR = personaR.FechaNacimientoR,
+                    SueldoR = personaR.SueldoR,
+                    EstatusR = personaR.EstatusR,
+                };
+            }
+            else
+                return new PersonaR();
+        }
+
+        public async Task<List<PersonaR>> ObtenerTodosAsync()
+        {
+            var personaR = await dbContext.PersonaR.ToListAsync();
+            if (personaR != null && personaR.Count > 0)
+            {
+                var list = new List<PersonaR>();
+                personaR.ForEach(p => list.Add(new PersonaR
+                {
+                    Id = p.Id,
+                    NombreR = p.NombreR,
+                    ApellidoR = p.ApellidoR,
+                    FechaNacimientoR = p.FechaNacimientoR,
+                    SueldoR = p.SueldoR,
+                    EstatusR = p.EstatusR
+                }));
+                return list;
+            }
+            else
+                return new List<PersonaR>();
         }
     }
 }
---
+++ R.LogicaNegocio/PersonaRBL.cs
@@ -0,0 +1,44 @@
+﻿using R.AccesoDatos;
+using R.EntidadesNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R.LogicaNegocio
+{
+    public class PersonaRBL
+    {
+        readonly PersonaRDAL personaRDAL;
+        public PersonaRBL(PersonaRDAL pPersonaRDAL)
+        {
+            personaRDAL = pPersonaRDAL;
+        }
+
+        public async Task<int> CrearAsync(PersonaR pPersonaR)
+        {
+            return await personaRDAL.CrearAsync(pPersonaR);
+        }
+
+        public async Task<int> ModificarAsync(PersonaR pPersonaR)
+        {
+            return await personaRDAL.ModificarAsync(pPersonaR);
+        }
+
+        public async Task<int> EliminarAsync(PersonaR pPersonaR)
+        {
+            return await personaRDAL.EliminarAsync(pPersonaR);
+        }
+
+        public async Task<PersonaR> ObtenerPorIdAsync(PersonaR pPersonaR)
+        {
+            return await personaRDAL.ObtenerPorIdAsync(pPersonaR);
+        }
+
+        public async Task<List<PersonaR>> ObtenerTodosAsync()
+        {
+            return await personaRDAL.ObtenerTodosAsync();
+        }
+    }
+}
